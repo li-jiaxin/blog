e9b5291a9afd4da0b84e45bbfa02cd34f6a69abe
meimeida <1412311510@qq.com>
2015-04-09 17:39:11 +0800

    index

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>index.html</title>
+	<link rel="stylesheet" href="static/bootstrap/dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="static/css/global.css" />
+</head>
+<body class="index-body">
+	<div class="index-cover"></div>
+	<div class="index-content">
+	   <div>
+	       <a title="前往 apgmer 的主页" href="/">
+           <img class="index-logo" width="80" alt="apgmer logo" src="static/image/logo2.jpg">
+           </a>
+           <h1>
+           <a title="link to homepage for myself" href="/">myself</a>
+           </h1>
+           <span class="index-name">小小程序员</span>
+           <hr class="">
+	   </div> 
+	</div>
+</body>
+</html>
