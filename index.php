b6db481112eeab44cbded46c64fdebe9b2e6b27a
meimeida <1412311510@qq.com>
2015-04-10 10:55:25 +0800

    index

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>index.html</title>
 	<link rel="stylesheet" href="static/bootstrap/dist/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="static/css/global.css" />
+	<link rel="stylesheet" href="static/font-awesome/css/font-awesome.min.css" />
 </head>
 <body class="index-body">
 	<div class="index-cover"></div>
@@ -16,8 +17,42 @@
            <h1>
            <a title="link to homepage for myself" href="/">myself</a>
            </h1>
-           <span class="index-name">小小程序员</span>
-           <hr class="">
+           <span class="index-name">小小的我</span>
+           <hr class="cover-one">
+           <p class="index-description">么么哒</p>
+           <hr class="cover-two">
+           <div>
+                <ul class="list-inline">
+                   <li class="navigation__item">
+                      <a class="blog-button" title="访问博客" href="/#blog">博客</a>
+                   </li>
+                   <li class="navigation__item">
+                      <a class="blog-button" href="/projects">项目作品</a>
+                   </li>
+                   <li class="navigation__item">
+                      <a class="blog-button" href="/aboutme">关于我</a>
+                   </li>
+                 </ul>
+           </div>
+           <div>
+                <ul class="list-inline">
+                    <li>
+                     <a href="">
+                        <i class="fa fa-mortar-board fa-2x"></i>
+                     </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <i class="fa fa-github-square fa-2x"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <i class="fa fa-diamond fa-2x"></i>
+                      </a>
+                    </li>
+                </ul>
+           </div>
 	   </div> 
 	</div>
 </body>
